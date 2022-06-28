86cde73a667fed34eac156ab2fc5e3c76102905d
Yash Ambekar <77171308+yashambkr@users.noreply.github.com>
2022-06-28 23:05:52 +0530

    First commit: Fitness-111 a React based web application implemented to fetch the different exercise with web based rapid api and youtube api to show the video related to similar topics

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo-1.png';
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#000000">
+    <Stack color="#fff" gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={Logo} alt="logo" style={{ width: '200px', height: '200px' }} />
+    </Stack>
+    </Box>
+);
+
+export default Footer;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,24 @@
+export const exerciseOptions=  {
+  method: 'GET',
+  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+  headers: {
+    
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'dbdc65f0f5msh7969aa1b23fe152p11e1c5jsn83e22f105890'
+  }
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85'
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
